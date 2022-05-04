26c89fbef2acfc196fe1286232b8b7a3ad83e60e
Dahonglin <silversky88@naver.com>
2022-05-04 15:29:46 +0900

    중복여부

--- js/main.js
+++ js/main.js
@@ -26,19 +26,38 @@ window.addEventListener("load", () => {
         inputMenu: "",
         picked: "",
         // 임의 이전 랜덤값
-        prePicked : 999,
+        prePicked: 999,
         // 모달창 열기/닫기
-        resultPopup : 0
+        resultPopup: 0
       };
     }, //data
     methods: {
       // 메뉴 추가
       addNewMenu() {
         // 메뉴 배열에 추가함
-        if (this.inputMenu != ''){
-          this.menu.push(this.inputMenu);
-        }
-        else {
+        if (this.inputMenu != '') {
+          // 중복 체크 부분
+
+          //중복 유무
+          let chk = true;
+          //메뉴 데이터를 확인 반복문
+          for (let i = 0; i < this.menu.length; i++) {
+            //중복유무 초기화
+            chk = true;
+            // 중복된 데이터가 있는지 확인
+            if (this.menu[i] === this.inputMenu) {
+              console.log('중복됨');
+              //중복 경고창 알림
+              window.alert('이미 등록 된 메뉴입니다!');
+              chk = false;
+              // 인풋 태그 초기화
+              this.inputMenu = "";
+              return false;
+            }
+          }
+          if (chk)
+            this.menu.push(this.inputMenu);
+        } else {
           return false;
         }
         // 인풋 태그 초기화
@@ -49,17 +68,22 @@ window.addEventListener("load", () => {
         // 랜덤 추첨
         let random = Math.floor(Math.random() * this.menu.length);
         // 이전 값과 같으면 다를 때까지 계속 랜덤 수 생성
-        while (random === this.prePicked){
+        while (random === this.prePicked) {
           random = Math.floor(Math.random() * this.menu.length);
         }
         // 이전 숫자에 현재 랜덤 수 더해줌
         this.prePicked = random;
         // 추첨 완료
         this.picked = this.menu[random];
-        console.log(this.picked);
       },
       closeWindow(val) {
         this.resultPopup = val;
+      },
+      // 메뉴 삭제
+      deleteMenu(val) {
+        if (val > -1) {
+          this.menu.splice(val, 1);
+        }
       }
     }
   }); //뷰
